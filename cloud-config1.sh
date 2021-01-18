bc2c08889b14f96215f26e00d40a23f137e5d4be
diogocarreira01 <72440669+diogocarreira01@users.noreply.github.com>
2021-01-18 13:33:35 -0100

    Update cloud-config1.sh

--- cloud-config1.sh
+++ cloud-config1.sh
@@ -12,61 +12,27 @@ source ./logger.sh
 
 SCRIPTENTRY
 DEBUG "Script starting."
-
+sudo sh -c 'echo root:Passw0rd | chpasswd'
 sudo apt-get update && sudo apt-get -y upgradecloud-config.sh
 INFO "System updated and upgraded."
 
-sudo apt-get -y install apache2 mysql-server php libapache2-mod-php php-mysql debconf-utils php-mbstring php-intl php-soap php-xml php-xmlrpc build-essential
+sudo apt-get -y install apache2
 INFO "Software packages installed."
-
-cd /var/www
-sudo git clone git://git.moodle.org/moodle.git
-cd moodle
-sudo git branch --track MOODLE_39_STABLE origin/MOODLE_39_STABLE
-sudo git checkout MOODLE_39_STABLE
-INFO "Moodle installed."
-
-cd /var/www
-sudo git clone https://github.com/joomla/joomla-cms.git
-INFO "Joomla installed."
-
-echo "<?php" >/tmp/info.php
-echo "phpinfo();" >>/tmp/info.php
-echo "?>" >>/tmp/info.php
-
-sudo cp /tmp/info.php /var/www/joomla
-sudo cp /tmp/info.php /var/www/moodle
-rm /tmp/info.php
-INFO "File info.php created at the Joomla and Moodle roots."
-
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Passw0rd';" >/tmp/script.sql
-echo "FLUSH PRIVILEGES;" >>/tmp/script.sql
-
-sudo mysql </tmp/script.sql
-rm /tmp/script.sql
-INFO "MySQL root user updated with a password and mysql_native_password authentication."
-
-echo "echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections" >/tmp/script.sh
-echo "echo 'phpmyadmin phpmyadmin/app-password-confirm password Passw0rd' | debconf-set-selections" >>/tmp/script.sh
-echo "echo 'phpmyadmin phpmyadmin/mysql/admin-pass password Passw0rd' | debconf-set-selections" >>/tmp/script.sh
-echo "echo 'phpmyadmin phpmyadmin/mysql/app-pass password Passw0rd' | debconf-set-selections" >>/tmp/script.sh
-echo "echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections" >>/tmp/script.sh
-
-sudo sh /tmp/script.sh
-sudo apt-get -y install phpmyadmin
-rm /tmp/script.sh
-INFO "PHPMyAdmin installed."
-
+sudo a2enmod ssl
+sudo a2ensite default-ssl.conf
+sudo systemctl restart apache2
+sudo systemctl enable apache2
 sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/001-default.conf
 sudo sed -i -e 's/:80/:8080/g' /etc/apache2/sites-available/001-default.conf
-sudo sed -i -e 's/html/moodle/g' /etc/apache2/sites-available/001-default.conf
-sudo sed -i -e 's/html/joomla-cms/g' /etc/apache2/sites-available/000-default.conf
+sudo sed -i -e 's/:443/:8443/g' /etc/apache2/sites-available/default-ssl.conf
 sudo chown -R www-data:www-data /var/www/*
-sudo a2ensite 001-default.conf
 sudo sed -i -e 's/Listen 80/Listen 80\nListen 8080/g' /etc/apache2/ports.conf
+sudo sed -i -e 's/Listen 443/Listen 443\nListen 8443/g' /etc/apache2/ports.conf
+sudo mkdir "/var/www/htmls"
+sudo sed -i -e 's/html/htmls/g' /etc/apache2/sites-available/default-ssl.conf
+sudo touch /var/www/htmls/index.html
 sudo systemctl restart apache2
 INFO "Web sited created. Joomla on port 80 and Moodle on port 8080."
-
 DEBUG "Script reached the end."
 SCRIPTEXIT
 
